06aeb535ea38a44172b40178436fb8c68345ed5f
Jason C. Daniels <jason.c.daniels@gmail.com>
2021-10-16 04:07:38 -0500

    Project created with jcd-new classlib

---
+++ Jcd.DoWhenRun.Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jcd.DoWhenRun.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Jcd.DoWhenRun/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Jcd.DoWhenRun
+{
+    public class Class1
+    {
+    }
+}
